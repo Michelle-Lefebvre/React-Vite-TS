be5e350c1e6d3705744d4abf2b36f4bd2ade6a3a
Michelle Lefebvre <mmldesigns007@gmail.com>
2025-10-17 14:27:48 -0400

    ListGroup component

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 React app built using Vite, TypeScript, Bootstrap
 To start:
 
-##Install
+###Install
 Create App:
 _npm create vite@4.1.0_
 
@@ -19,5 +19,14 @@ _npm run dev_
 Bootstrap:
 _npm i bootstrap@5.2.3_
 
-##VS Code tricks
+Bootstrap Components
+List group:
+
+<ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+
+###VS Code tricks
 _CMD + D_ // to select multiple text
--- react-vite-ts/src/components/ListGroup.tsx
+++ react-vite-ts/src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
 function ListGroup() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+  //   items = []
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item active" aria-current="true">
-        An active item
-      </li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found.</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 export default ListGroup
